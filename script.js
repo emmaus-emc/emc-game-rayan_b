6b4838f523e2bb1306eff5566ab749835e4d5db7
Rayan Benlakdame <74540117+RayanBe@users.noreply.github.com>
2021-11-25 15:06:48 +0000

    speler wordt laten zien in eindscherm, geeft een leuk effect vind ik

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ var spelStatus = STARTSCHERM;
 
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
-var vijandX = 600
-var vijandY = 0
+var vijandX1 = 900
+var vijandY1 = 0
+var vijandX2 = 300
+var vijandY2 = 0
+var vijandX3 = 600
+var vijandY3 = 0
 var KEY_LEFT = 37
 var KEY_RIGHT = 39
 var KEY_UP = 38
@@ -34,8 +38,10 @@ var punten = 0
  * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
 var beweegAlles = function () {
-  // vijand
-  vijandY = vijandY + 10
+  // vijanden
+  vijandY1 = vijandY1 + 10
+  vijandY2 = vijandY2 + 10
+  vijandY3 = vijandY3 + 10
   // kogel
 
   // speler
@@ -78,18 +84,44 @@ var verwerkBotsingEnPunten = function () {
   }
 
   // botsing vijand tegen barrier
-  if (vijandY >= 720) {
-    vijandY = 0
+  if (vijandY1 >= 720) {
+    vijandY1 = 0
     let numbers = ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100'];
-    vijandX = random(numbers);
+    vijandX1 = random(numbers);
+  }
+  if (vijandY2 >= 720) {
+    vijandY2 = 0
+    let numbers = ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100'];
+    vijandX2 = random(numbers);
+  }
+  if (vijandY3 >= 720) {
+    vijandY3 = 0
+    let numbers = ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100'];
+    vijandX3 = random(numbers);
   }
 
   // botsing speler tegen vijand
 
-  if ((spelerX - vijandX) < 52 &&
-    (spelerX - vijandX) > -52 &&
-    (spelerY - vijandY) < 52 &&
-    (spelerY - vijandY) > -52) {
+  if ((spelerX - vijandX1) < 52 &&
+    (spelerX - vijandX1) > -52 &&
+    (spelerY - vijandY1) < 52 &&
+    (spelerY - vijandY1) > -52) {
+    console.log("botsing")
+    hp = hp - 1
+  }
+
+  if ((spelerX - vijandX2) < 52 &&
+    (spelerX - vijandX2) > -52 &&
+    (spelerY - vijandY2) < 52 &&
+    (spelerY - vijandY2) > -52) {
+    console.log("botsing")
+    hp = hp - 1
+  }
+
+  if ((spelerX - vijandX3) < 52 &&
+    (spelerX - vijandX3) > -52 &&
+    (spelerY - vijandY3) < 52 &&
+    (spelerY - vijandY3) > -52) {
     console.log("botsing")
     hp = hp - 1
   }
@@ -139,26 +171,67 @@ var tekenAlles = function () {
     createCanvas(1280, 720);
     background(0, 55, 133);
   }
-  if (vijandY) {
+
+  if (vijandY1) {
     createCanvas(1280, 720);
     background(0, 55, 133);
   }
 
-  // vijand
+  if (vijandY2) {
+    createCanvas(1280, 720);
+    background(0, 55, 133);
+  }
+
+  if (vijandY3) {
+    createCanvas(1280, 720);
+    background(0, 55, 133);
+  }
+
+  // vijanden
   fill("white");
-  rect(vijandX - 25, vijandY - 25, 50, 50);
+  rect(vijandX1 - 25, vijandY1 - 25, 50, 50);
   fill("red");
-  rect(vijandX - 25, vijandY - 25, 12.5, 25);
+  rect(vijandX1 - 25, vijandY1 - 25, 12.5, 25);
   fill("red");
-  rect(vijandX + 12.5, vijandY - 25, 12.5, 25);
+  rect(vijandX1 + 12.5, vijandY1 - 25, 12.5, 25);
   fill("red");
-  rect(vijandX - 25, vijandY + 40 - 25, 50, 10);
+  rect(vijandX1 - 25, vijandY1 + 40 - 25, 50, 10);
   fill("black");
-  rect(vijandX + 12.5, vijandY + 12.50 - 25, 10, 10);
+  rect(vijandX1 + 12.5, vijandY1 + 12.50 - 25, 10, 10);
   fill("black");
-  rect(vijandX - 25, vijandY + 12.5 - 25, 10, 10);
+  rect(vijandX1 - 25, vijandY1 + 12.5 - 25, 10, 10);
   fill("purple");
-  ellipse(vijandX, vijandY, 5, 5);
+  ellipse(vijandX1, vijandY1, 5, 5);
+
+  fill("white");
+  rect(vijandX2 - 25, vijandY2 - 25, 50, 50);
+  fill("red");
+  rect(vijandX2 - 25, vijandY2 - 25, 12.5, 25);
+  fill("red");
+  rect(vijandX2 + 12.5, vijandY2 - 25, 12.5, 25);
+  fill("red");
+  rect(vijandX2 - 25, vijandY2 + 40 - 25, 50, 10);
+  fill("black");
+  rect(vijandX2 + 12.5, vijandY2 + 12.50 - 25, 10, 10);
+  fill("black");
+  rect(vijandX2 - 25, vijandY2 + 12.5 - 25, 10, 10);
+  fill("purple");
+  ellipse(vijandX2, vijandY2, 5, 5);
+
+  fill("white");
+  rect(vijandX3 - 25, vijandY3 - 25, 50, 50);
+  fill("red");
+  rect(vijandX3 - 25, vijandY3 - 25, 12.5, 25);
+  fill("red");
+  rect(vijandX3 + 12.5, vijandY3 - 25, 12.5, 25);
+  fill("red");
+  rect(vijandX3 - 25, vijandY3 + 40 - 25, 50, 10);
+  fill("black");
+  rect(vijandX3 + 12.5, vijandY3 + 12.50 - 25, 10, 10);
+  fill("black");
+  rect(vijandX3 - 25, vijandY3 + 12.5 - 25, 10, 10);
+  fill("purple");
+  ellipse(vijandX3, vijandY3, 5, 5);
 
   // kogel
 
@@ -260,6 +333,21 @@ function draw() {
 
     textSize(100);
     text(floor(punten), 1100, 100);
+  
+    fill("white");
+    rect(spelerX - 25, spelerY - 25, 50, 50);
+    fill("black");
+    ellipse(spelerX, spelerY, 10, 10);
+    fill("black");
+    rect(spelerX - 7.5, spelerY - 25, 15, 20);
+    fill("gray");
+    rect(spelerX - 25, spelerY, 12.5, 25);
+    fill("gray");
+    rect(spelerX + 12.5, spelerY, 12.5, 25);
+    fill("red");
+    rect(spelerX - 15, spelerY - 5, 12.5, 5);
+    fill("red");
+    rect(spelerX + 2.5, spelerY - 5, 12.5, 5);
 
     // teken game-over scherm
 
